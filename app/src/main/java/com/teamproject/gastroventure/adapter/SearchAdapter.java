d75af2457e96c91c9bb45b12b0c49456a4cc1248
koreajava <dzmanyo@naver.com>
2020-03-16 17:35:55 +0900

    Merge branch 'master' of C:\Users\82108\AndroidStudioProjects\Gastroventure-Team with conflicts.

---
+++ app/src/main/java/com/teamproject/gastroventure/adapter/SearchAdapter.java
@@ -0,0 +1,7 @@
+package com.teamproject.gastroventure.adapter;
+
+/**
+ * Created by 82108 on 2020-03-16.
+ */
+public class SearchAdapter {
+}
---
+++ app/src/main/java/com/teamproject/gastroventure/vo/SearchVo.java
@@ -0,0 +1,7 @@
+package com.teamproject.gastroventure.vo;
+
+/**
+ * Created by 82108 on 2020-03-16.
+ */
+public class SearchVo {
+}
